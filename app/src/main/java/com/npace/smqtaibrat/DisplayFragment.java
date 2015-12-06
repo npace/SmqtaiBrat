10e76ac0c2eaed5bef6391e6438d701b2d7ab39a
Lubomir Naydenov <lubonay@gmail.com>
2015-12-06 11:32:44 +0200

    Added DisplayFragmentTest

---
+++ app/src/main/java/com/npace/smqtaibrat/DisplayFragment.java
@@ -0,0 +1,7 @@
+package com.npace.smqtaibrat;
+
+/**
+ * Created by npace on 006, 06-Dec-15.
+ */
+public class DisplayFragment {
+}
---
+++ app/src/test/java/com/npace/smqtaibrat/DisplayFragmentTest.java
@@ -0,0 +1,15 @@
+package com.npace.smqtaibrat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by npace on 006, 06-Dec-15.
+ */
+public class DisplayFragmentTest {
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(new DisplayFragment());
+    }
+}
