e113e06e7adfdee84971c8c4b27c0b277045fb12
Lubomir Naydenov <lubonay@gmail.com>
2015-12-06 11:43:33 +0200

    Added dependency on robolectric support shadows, made the fragment actually extend support.v4.fragment

--- app/src/main/java/com/npace/smqtaibrat/DisplayFragment.java
+++ app/src/main/java/com/npace/smqtaibrat/DisplayFragment.java
@@ -1,7 +1,10 @@
 package com.npace.smqtaibrat;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by npace on 006, 06-Dec-15.
  */
-public class DisplayFragment {
+public class DisplayFragment extends Fragment {
+    
 }
--- app/src/test/java/com/npace/smqtaibrat/DisplayFragmentTest.java
+++ app/src/test/java/com/npace/smqtaibrat/DisplayFragmentTest.java
@@ -1,15 +1,21 @@
 package com.npace.smqtaibrat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.assertNotNull;
+import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 /**
  * Created by npace on 006, 06-Dec-15.
  */
+@RunWith(RobolectricTestRunner.class)
 public class DisplayFragmentTest {
     @Test
     public void shouldNotBeNull() throws Exception {
-        assertNotNull(new DisplayFragment());
+        DisplayFragment fragment = new DisplayFragment();
+        startFragment(fragment);
+        assertNotNull(fragment);
     }
 }
