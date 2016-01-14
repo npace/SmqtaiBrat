53d2160c3472147be97eaddc823994542114e35d
Lubomir Naydenov <lubonay@gmail.com>
2016-01-14 08:41:30 +0200

    Added ButtonsFragment and test class

---
+++ app/src/main/java/com/npace/smqtaibrat/ButtonsFragment.java
@@ -0,0 +1,12 @@
+package com.npace.smqtaibrat;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by npace on 006, 06-Dec-15.
+ */
+public class ButtonsFragment extends Fragment {
+    public static ButtonsFragment newInstance() {
+        return new ButtonsFragment();
+    }
+}
